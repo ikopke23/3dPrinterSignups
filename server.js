e283bfa020d7e0892779c70825f8f8189dd1b698
ikopke23 <ian.kopke23@trinityschoolnyc.org>
2023-01-13 06:41:56 -0800

    Print details works
    Still issues with the printers detailed perspectives

--- server.js
+++ server.js
@@ -97,12 +97,13 @@ app.post('/addprinter', function(request, response){
 });
 
 app.get('/printer/:printerName', function(request, response){
-  let printers = JSON.parse(fs.readFileSync("data/printer.JSON"))
+  let printers = JSON.parse(fs.readFileSync("data/printers.json"))
   // console.log("/print/:printName")
   let printerName = request.params.printName;
   // console.log("printName = "+printName)
   // console.log("prints = "+prints[printName]["description"])
   if(printers[printerName]){
+    console.log("This is a real print")
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
     response.render("printerDetails", {
@@ -180,6 +181,7 @@ app.get('/print/:printName', function(request, response){
   // console.log("printName = "+printName)
   // console.log("prints = "+prints[printName]["description"])
   if(prints[printName]){
+    console.log("it's the if statement")
     response.status(200);
     response.setHeader('Content-Type', 'text/html')
     response.render("printDetails", {
@@ -205,7 +207,7 @@ app.post('/printcreate', function(request,response){
   // console.log(prints);
   // console.log("PrintName =  "+ printName);
   // console.log(request.body.width)
-  if(request.body.printName && request.body.description && request.body.link && request.body.infill && request.body.width && request.body.time && request.body.printer){
+  if(request.body.printName && request.body.description && request.body.link && request.body.infill && request.body.width && request.body.time && request.body.printer && request.body.photo){
     let newPrint = {
       "name":printName,
       "description":request.body.description,
@@ -215,6 +217,7 @@ app.post('/printcreate', function(request,response){
       "width":request.body.width,
       "studentName":request.body.studentName,
       "printer":request.body.printer,
+      "photo":request.body.photo,
     };
     console.log("newPrint = ");
     console.log(newPrint)
@@ -248,149 +251,7 @@ app.get('/printDetails', function(request, response){
 });
 
 
-
-
-//------------------- DEMO CODE BELOW -------------------------------
-
-app.get('/play', function(request, response) {
-    let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("play", {
-      data: opponents
-    });
-});
-
-app.get('/results', function(request, response) {
-    let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-
-    //accessing URL query string information from the request object
-    let opponent = request.query.opponent;
-    let playerThrow = request.query.throw;
-
-    if(opponents[opponent]){
-      let opponentThrowChoices=["Paper", "Rock", "Scissors"];
-      let results={};
-
-      results["playerThrow"]=playerThrow;
-      results["opponentName"]=opponent;
-      results["opponentPhoto"]=opponents[opponent].photo;
-      results["opponentThrow"] = opponentThrowChoices[Math.floor(Math.random() * 3)];
-
-      if(results["playerThrow"]===results["opponentThrow"]){
-        results["outcome"] = "tie";
-      }else if(results["playerThrow"]==="Paper"){
-        if(results["opponentThrow"]=="Scissors") results["outcome"] = "lose";
-        else results["outcome"] = "win";
-      }else if(results["playerThrow"]==="Scissors"){
-        if(results["opponentThrow"]=="Rock") results["outcome"] = "lose";
-        else results["outcome"] = "win";
-      }else{
-        if(results["opponentThrow"]=="Paper") results["outcome"] = "lose";
-        else results["outcome"] = "win";
-      }
-
-      if(results["outcome"]=="lose") opponents[opponent]["win"]++;
-      else if(results["outcome"]=="win") opponents[opponent]["lose"]++;
-      else opponents[opponent]["tie"]++;
-
-      //update opponents.json to permanently remember results
-      fs.writeFileSync('data/opponents.json', JSON.stringify(opponents));
-
-      response.status(200);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("results", {
-        data: results
-      });
-    }else{
-      response.status(404);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("error", {
-        "errorCode":"404"
-      });
-    }
-});
-
-app.get('/scores', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-  let opponentArray=[];
-
-  //create an array to use sort, and dynamically generate win percent
-  for(name in opponents){
-    opponents[name].win_percent = (opponents[name].win/parseFloat(opponents[name].win+opponents[name].lose+opponents[name].tie) * 100).toFixed(2);
-    if(opponents[name].win_percent=="NaN") opponents[name].win_percent=0;
-    opponentArray.push(opponents[name])
-  }
-  opponentArray.sort(function(a, b){
-    return parseFloat(b.win_percent)-parseFloat(a.win_percent);
-  })
-
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("scores",{
-    opponents: opponentArray
-  });
-});
-
-app.get('/opponent/:opponentName', function(request, response) {
-  let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-
-  // using dynamic routes to specify resource request information
-  let opponentName = request.params.opponentName;
-
-  if(opponents[opponentName]){
-    opponents[opponentName].win_percent = (opponents[opponentName].win/parseFloat(opponents[opponentName].win+opponents[opponentName].lose+opponents[opponentName].tie) * 100).toFixed(2);
-    if(opponents[opponentName].win_percent=="NaN") opponents[opponentName].win_percent=0;
-
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("opponentDetails",{
-      opponent: opponents[opponentName]
-    });
-
-  }else{
-    response.status(404);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("error", {
-      "errorCode":"404"
-    });
-  }
-});
-
-app.get('/opponentCreate', function(request, response) {
-
-    response.status(200);
-    response.setHeader('Content-Type', 'text/html')
-    response.render("opponentCreate");
-});
-
-app.post('/opponentCreate', function(request, response) {
-    let opponentName = request.body.opponentName;
-    let opponentPhoto = request.body.opponentPhoto;
-    if(opponentName&&opponentPhoto){
-      let opponents = JSON.parse(fs.readFileSync('data/opponents.json'));
-      let newOpponent={
-        "name": opponentName,
-        "photo": opponentPhoto,
-        "win":0,
-        "lose": 0,
-        "tie": 0,
-      }
-      opponents[opponentName] = newOpponent;
-      fs.writeFileSync('data/opponents.json', JSON.stringify(opponents));
-
-      response.status(200);
-      response.setHeader('Content-Type', 'text/html')
-      response.redirect("/opponent/"+opponentName);
-    }else{
-      response.status(400);
-      response.setHeader('Content-Type', 'text/html')
-      response.render("error", {
-        "errorCode":"400"
-      });
-    }
-});
-
+//------------------- DEMO CODE BELOW ------------------------------
 // Because routes/middleware are applied in order,
 // this will act as a default error route in case of
 // a request fot an invalid route
