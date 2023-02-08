84bae764815407cfe842b216bc28ddf2ccd13f76
ikopke23 <90466377+ikopke23@users.noreply.github.com>
2023-02-08 09:42:21 -0500

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,49 @@
 const express = require('express'),
   router = express.Router();
 
-router.get('/', function(request, response) {
-  response.status(200);
-  response.setHeader('Content-Type', 'text/html')
-  response.render("index");
-});
 
+  router.get('/', function(request, response) {
+    let hofPrints = JSON.parse(fs.readFileSync('data/hofprints.json'));
+    let keys = Object.keys(hofPrints)
+    let randomKey = Math.floor(Math.random()*keys.length);
+    let print = hofPrints[keys[randomKey]]
+    console.log(print)
+    const date = new Date();
+    console.log(date)
+  
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html');
+    response.render("index", {
+      hofPrint :print,
+      printers: JSON.parse(fs.readFileSync('data/printers.json'))
+  
+    })
+  });
+  
+  router.get('/guide', function(request, response) {
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("guide")
+  });
+  
+  router.get('/advtips', function(request, response){
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("advTips")
+  })
+  
+  router.get('/schedule', function(request, response){
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("schedule")
+  });
+  
+  router.get('/status', function(request, response){
+    response.status(200);
+    response.setHeader('Content-Type', 'text/html')
+    response.render("status")
+  });
+  
 router.get('/error', function(request, response) {
   const errorCode = request.query.code;
   if (!errorCode) errorCode = 400;
